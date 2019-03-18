a1c1d1dd775cbff5b5b9aa895554623827ac0fbf
Mannan Qazi <mannanqazi2@gmail.com>
2019-03-18 00:16:53 -0400

    Re-factored assets folder

---
+++ assets/JS/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // when page loads up
+  // create grid layout of class box
+  for (var i = 0; i < 16; i++) {
+    for (var j = 0; j < 16; j++) {
+      $('.container').append('<div class="box"></div>');
+    }
+  }
+
+  // Then, when user hovers over the element ('this' box) the active selector is added)
+  $('.box').mouseover(function() {
+    $(this).addClass('active');
+  });
+
+  // Clear everything
+  document.querySelector('.btn').addEventListener('click', function() {
+    $('.box').removeClass('active');
+    //console.log('clicked');
+  });
+});
